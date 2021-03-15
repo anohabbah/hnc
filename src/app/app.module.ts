850bf5ec93a5594b3c1c1e6db8538a8aad3a5ff8
Abbah Anoh <anohabbah@gmail.com>
2021-03-15 23:32:06 +0100

    feat: 🎸 implement item service load method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/services/item/item.service.ts
+++ src/app/services/item/item.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { Items } from '@hnc/models/item.interface';
+import { combineLatest, Observable } from 'rxjs';
+import { Item, Items } from '@hnc/models/item.interface';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Injectable()
 export class ItemService {
+  constructor(private db: AngularFireDatabase) {}
+
   load(offset: number, limit: number): Observable<Items> {
-    return of({
-      offset: 0,
-      limit: 0,
-      total: 0,
-      results: []
-    });
+    return this.db.list('/v0/stories').valueChanges().pipe(
+      mergeMap((ids: any[]) => combineLatest(ids.map(id => this.db.object('/v0/item/' + id).valueChanges()))),
+      map(items => ({
+        offset,
+        limit,
+        total: limit,
+        results: items as Item[],
+      }))
+    );
   }
 }
