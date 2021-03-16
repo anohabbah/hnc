3abb215532e959d3ac40cc39ffb7cdf70e35e27c
Abbah Anoh <anohabbah@gmail.com>
2021-03-16 22:59:56 +0100

    Updated template of ItemComponent

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,13 +7,14 @@ import {ItemsComponent} from '@hnc/components/items/items.component';
 import {IonicModule} from '@ionic/angular';
 import {CommentComponent} from '@hnc/components/comment/comment.component';
 import {CommentListComponent} from '@hnc/components/comment-list/comment-list.component';
+import {RouterModule} from '@angular/router';
 
 const DECLARATIONS = [ItemComponent, ItemsComponent, CommentComponent, CommentListComponent];
 
 @NgModule({
   declarations: [...DECLARATIONS, TimeAgoPipe],
   imports: [
-    CommonModule, IonicModule,
+    CommonModule, IonicModule, RouterModule,
   ],
   exports: [...DECLARATIONS]
 })
